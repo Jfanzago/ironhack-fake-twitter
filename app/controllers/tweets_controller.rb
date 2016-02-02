2c94cc7d9db969786cd6cd3d3163df06e2240c27
jfanzago <jfanzago@gmail.com>
2016-02-02 17:55:06 -0500

    initial fake twitter commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,9 @@
+class TweetsController < ApplicationController
+	def index
+		@tweets = Tweet.all
+	
+		render "index"
+	end
+		
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+ 
+
+ get "/" => "tweets#index"
+
+
+
+
+
+
+
+
+end
